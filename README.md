df770071c8ecb3b5c9609b74833d38a826cee29a
Daniel Rampanelli <hello@danielrampanelli.com>
2018-12-03 10:51:46 +0100

    Add snapshot of the basic mail dispatcher implementation based on the prototype.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# mail-dispatcher
-The repository containa a mail dispatcher developed using amazon AWS technologies (SES and Lambda functions) in order to map and forward mails sent to aliases or mailing lists.
+# MAIL DISPATCHER
+
+## Description
+
+The repository contains a mail dispatcher developed using Amazon AWS technologies ([SES](https://aws.amazon.com/ses/), [Lambda](https://aws.amazon.com/lambda/)) in order to map and forward mails sent to aliases or mailing lists.
+
+## Requirements
+
+This application requires
+
+- Node.js (8.10 or greater)
+- Unix-like environment (Linux/Mac OS X)
+- Shell/Terminal
+- AWS Account
+
+## Setup
+
+The following instructions will configure the environment and need to be performed only once, unless of course, the configuration/environment is changed significantly.
+
+1. Clone or download project repository
+1. Run `npm install` from the project's directory
+1. Create/configure certificates and roles in AWS
+1. Create configuration file `config.json`
+1. Create/update repository with mappings configuration
+1. Configure roles, verified domains and rules for incoming emails
+
+## Usage
+
+The `mail-dispatcher` executable provides multiple sub-commands, namely
+
+* help
+* deploy
+
+The following options are available for all sub-commands
+
+	-c, --configuration    Path to external/different configuration file to use
+
+If no external configuration file is specified using `-c` or `--configuration`, then the application expects to find a configuration file named **config.json** inside the project folder.
+
+### mail-dispatcher help
+
+Print all the application's sub-commands and available options.
+
+### mail-dispatcher deploy
+
+Fetch the mappings configuration and deploy the function to the AWS infrastructure.
---
+++ src/mail-dispatcher.js
@@ -0,0 +1,173 @@
+const async = require('async')
+const AWS = require('aws-sdk')
+const child_process = require('child_process')
+const fs = require('fs-extra')
+const globby = require('globby')
+const tmp = require('tmp')
+const winston = require('winston')
+
+module.exports = class MailDispatcher {
+
+    constructor(config) {
+        const self = this
+
+        self.configuration = config
+
+        if (!self.configuration.resourceName) {
+            self.configuration.resourceName = 'mail-dispatcher'
+        }
+
+        // TODO validate configuration
+
+        AWS.config.update({
+            region: self.configuration.aws.region,
+            credentials: {
+                accessKeyId: self.configuration.aws.accessKey,
+                secretAccessKey: self.configuration.aws.secretKey,
+                region: self.configuration.aws.region
+            }
+        })
+
+        self.logger = winston.createLogger({
+            level: 'info',
+            silent: false,
+            transports: [ new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.splat(),
+                    winston.format.simple()
+                )
+            }) ]
+        })
+    }
+
+    deploy() {
+        const self = this
+
+        var mappingsDirectory = tmp.dirSync()
+        var packageDir = tmp.dirSync()
+        var packageFile = tmp.tmpNameSync({ postfix: '.zip' })
+
+        async.waterfall([
+
+            (callback) => {
+                var functionConfiguration = {
+                    region: self.configuration.aws.region,
+                    bucket: self.configuration.aws.bucket,
+                    bucketPrefix: self.configuration.aws.bucketPrefix,
+                    mappings: {
+                        '@default': self.configuration.forwarding.defaultRecipient
+                    }
+                }
+
+                var matches
+
+                matches = self.configuration.mappings.match(/^git:\/\/(.+)$/)
+                if (!!matches) {
+                    self.logger.log('info', 'Fetching mappings configuration from repository "%s"...', matches[1])
+
+                    child_process.exec('git clone ' + matches[1] + ' ' + mappingsDirectory.name, () => {
+                        globby([ mappingsDirectory.name + '/**/*.json' ]).then(paths => {
+                            paths.forEach(path => {
+                                if (path.endsWith('.json')) {
+                                    const json = JSON.parse(fs.readFileSync(path))
+
+                                    if (!!json.address && !!json.recipients) {
+                                        functionConfiguration.mappings[json.address] = json.recipients
+                                    }
+                                }
+                            })
+
+                            return callback(null, functionConfiguration)
+                        })
+                    })
+                }
+            },
+
+            (functionConfiguration, callback) => {
+                self.logger.log('info', 'Creating function package...')
+
+                fs.copySync(__dirname + '/../node_modules', packageDir.name + '/node_modules')
+
+                fs.writeFileSync(packageDir.name + '/config.json', JSON.stringify(functionConfiguration))
+
+                fs.copySync(__dirname + '/index.js', packageDir.name + '/index.js')
+
+                child_process.exec('zip -rq -X "' + packageFile + '" ./node_modules ./config.json ./index.js', { cwd: packageDir.name }, () => {
+                    callback(null, functionConfiguration)
+                })
+            },
+
+            (functionConfiguration, callback) => {
+                self.logger.log('info', 'Deploying function "%s"...', self.configuration.resourceName)
+
+                const lambda = new AWS.Lambda()
+
+                lambda.getFunction({ FunctionName: self.configuration.resourceName }, (err, data) => {
+                    if (!!err && err.code !== 'ResourceNotFoundException') {
+                        self.logger.log('error', 'Lambda.getFunction', err)
+                        return callback(err)
+                    }
+
+                    const code = fs.readFileSync(packageFile).buffer
+
+                    if (!!data) {
+                        // TODO update function configuration
+
+                        lambda.updateFunctionCode({
+                            FunctionName: self.configuration.resourceName,
+                            Publish: true,
+                            ZipFile: code
+                        }, (err) => {
+                            if (!!err) {
+                                self.logger.log('error', 'Lambda.updateFunctionCode', err)
+                                return callback(err)
+                            }
+
+                            callback(null, functionConfiguration)
+                        })
+                    } else {
+                        lambda.createFunction({
+                            FunctionName: self.configuration.resourceName,
+                            Handler: 'index.handler',
+                            Runtime: 'nodejs8.10',
+                            MemorySize: 128,
+                            Publish: true,
+                            Role: self.configuration.aws.functionRoleArn,
+                            Timeout: 30,
+                            Code: {
+                                ZipFile: code
+                            }
+                        }, (err) => {
+                            if (!!err) {
+                                self.logger.log('error', 'Lambda.createFunction', err)
+                                return callback(err)
+                            }
+
+                            callback(null, functionConfiguration)
+                        })
+                    }
+                })
+            },
+
+            (functionConfiguration, callback) => {
+                self.logger.log('info', 'Cleaning up...')
+
+                fs.removeSync(mappingsDirectory.name)
+                fs.removeSync(packageDir.name)
+                fs.removeSync(packageFile)
+
+                callback(null, functionConfiguration)
+            }
+
+        ], (err) => {
+            if (!!err) {
+                console.error(err.message || err)
+                return process.exit(1)
+            }
+
+            self.logger.log('info', 'Deployment completed.')
+        })
+    }
+
+}
